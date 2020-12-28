db0885a7ced38a48f28fe85564784c121f11bff5
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2020-12-28 15:41:16 -0300

    [UPD] Atualizado o dataset do simple lstm, adicionado mais um valor para x e atualizado o input_shape

--- simple_LSTM.py
+++ simple_LSTM.py
@@ -1,8 +1,8 @@
 import time
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-from numpy import array
 import matplotlib.pyplot as plt
 
 
@@ -10,16 +10,22 @@ def getData():
     """
     Método responsável por montar os dados fake para previsão
     """
-    seq = [[0.0, 0.1], [0.1, 0.2], [0.2, 0.3], [0.3, 0.4], [0.4, 0.5]]
-    seq = array(seq)
-    x, y = seq[:, 0], seq[:, 1]
-    x = x.reshape((len(x), 1, 1))
-
-    print('x')
-    print(x.shape)
+    seq = np.array([
+        # [x1, x2,  x3,  x4,   y],
+        [0.0, 0.1, 0.4],
+        [0.1, 0.2, 0.5],
+        [0.2, 0.3, 0.6],
+        [0.3, 0.4, 0.7],
+        [0.4, 0.5, 0.8],
+    ])
+    x, y = seq[:, :2], seq[:, 2]
     print(x)
-    print('Y')
-    print(y.shape)
+    print(y)
+
+    x = x.reshape(x.shape[0], x.shape[1], 1)
+    print('x - shape: ' + str(x.shape))
+    print(x)
+    print('y - shape: ' + str(y.shape))
     print(y)
 
     return x, y
@@ -52,22 +58,22 @@ def main():
     print('Simples exemplo da utilização do modelo LSTM! ')
     inicio = time.time()
 
-    # cria o modelo
-    model = Sequential()
-    model.add(LSTM(10, input_shape=(1, 1)))
-    model.add(Dense(1, activation='linear'))
-
     # busca os dados
     x, y = getData()
 
+    # cria o modelo
+    model = Sequential()
+    model.add(LSTM(10, input_shape=(x.shape[1], x.shape[2])))
+    model.add(Dense(1, activation='linear'))
+
     # compila e fita o modelo
     model.compile(loss='mse', optimizer='adam')
-    model.fit(x, y, epochs=300)
+    model.fit(x, y, epochs=10)
 
     # monta o predict e apresenta os resultados
     previsao = model.predict(x, verbose=0)
     print(previsao)
-    info(y, previsao)
+    # info(y, previsao)
 
     fim = time.time()
     print('Tempo de execução: %.2f minutos' % ((fim - inicio) / 60))
