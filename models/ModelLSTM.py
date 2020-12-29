867a6ac9f22c96a90873a9564271cb4c8a4a33bd
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2020-12-29 10:39:30 -0300

    [UPD] Adicionado as métricas para o modelo LSTM

--- models/ModelLSTM.py
+++ models/ModelLSTM.py
@@ -2,7 +2,6 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import Sequential
-from keras.layers import Dense
 from keras.layers import LSTM
 from pathlib import Path
 from keras import backend
@@ -18,7 +17,7 @@ def rmseMetric(y_true, y_pred):
 
 class ModelLSTM:
     """
-    Classe que representa o modelo MLP
+    Classe que representa o modelo LSTM
     """
 
     # constantes das funções de ativação
@@ -59,42 +58,7 @@ class ModelLSTM:
         if len(layers) < 3:
             raise Exception('ERRO! ')
 
-        # instacia o modelo
-        self.model = Sequential()
-
-        # # insere a camada de entrada
-        # input_layer = layers.pop(0)
-        # self.model.add(Dense(
-        #     input_layer['qtd_neurons'],
-        #     kernel_initializer='uniform',
-        #     activation=input_layer['activation'],
-        #     input_dim=self.input_dimension,
-        # ))
-        #
-        # # insere as camadas intermediárias e de saída
-        # for layer in layers:
-        #     self.model.add(Dense(
-        #         layer['qtd_neurons'],
-        #         kernel_initializer='uniform',
-        #         activation=layer['activation'],
-        #     ))
-        #
-        # # Compilação do modelo com as métricas: R2, RMSE e MAPE
-        # self.model.compile(
-        #     loss='binary_crossentropy',
-        #     optimizer='adam',
-        #     metrics=['accuracy', rmseMetric, 'mape'],
-        # )
-
-        # self.model.add(LSTM(4, input_shape=(1, self.input_dimension)))
-        # self.model.add(Dense(1))
-        # self.model.compile(loss='mean_squared_error', optimizer='adam')
-
-        print('=> SHAPE ORIGINAL: ')
-        print(self.data['x_train'].shape)
-        print(self.data['y_train'].shape)
-
-        # realiza o reshape
+        # realiza o reshape dos dados para serem utilizados no modelo LSTM
         self.data['x_train'] = self.data['x_train'].reshape(
             self.data['x_train'].shape[0], self.data['x_train'].shape[1], 1
         )
@@ -108,26 +72,22 @@ class ModelLSTM:
             self.data['x'].shape[0], self.data['x'].shape[1], 1
         )
 
-        # create and fit the LSTM network
+        # instacia o modelo
         self.model = Sequential()
+
         self.model.add(LSTM(4, input_shape=(self.data['x_train'].shape[1], self.data['x_train'].shape[2])))
-        # self.model.add(Dense(1))
 
-        self.model.compile(loss='mean_squared_error', optimizer='adam')
-
-        # model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+        # compilação do modelo com as métricas: R2, RMSE e MAPE
+        self.model.compile(
+            loss='binary_crossentropy',
+            optimizer='adam',
+            metrics=['accuracy', rmseMetric, 'mape'],
+        )
 
     def __train(self):
         """
         Método responsável por realizar o treinamento e validação do modelo
         """
-        # print(len(self.data['x_train']))
-        # print(self.data['x_train'].shape)
-        # print(self.data['y_train'].shape)
-        # self.data['x_train'] = self.data['x_train'].reshape(self.data['x_train'].shape[0], self.data['x_train'].shape[1], 1)
-        # print(self.data['x_train'])
-        # print(self.data['x_train'].shape)
-
         history = self.model.fit(
             self.data['x_train'],
             self.data['y_train'],
@@ -142,18 +102,8 @@ class ModelLSTM:
         """
         Método responsável por realizar o teste do modelo
         """
-        print('passou 1 ... ')
-
-        print(self.data['x_test'])
-        print(self.data['y_test'])
-
         # avalia o modelo com os dados de teste
-        # loss, accuracy_model, rmse, mape = self.model.evaluate(self.data['x_test'], self.data['y_test'])
-        a = self.model.evaluate(self.data['x_test'], self.data['y_test'])
-
-        print(a)
-
-        print('passou 2 ... ')
+        loss, accuracy_model, rmse, mape = self.model.evaluate(self.data['x_test'], self.data['y_test'])
 
         # gera as detecções se cada notícia é fake ou não
         detections = self.model.predict(self.data['x'])
@@ -162,8 +112,7 @@ class ModelLSTM:
         rounded = [round(x[0]) for x in detections]
         accuracy_detection = np.mean(rounded == self.data['y'])
 
-        # return loss, accuracy_model, rmse, mape, accuracy_detection
-        return a
+        return loss, accuracy_model, rmse, mape, accuracy_detection
 
     def __result(self, history, loss, accuracy_model, rmse, mape, accuracy_detection):
         """
@@ -182,7 +131,7 @@ class ModelLSTM:
         :param accuracy_detection: Acurácia da detecção
         :type accuracy_detection: ndarray
         """
-        print('=> Modelo MLP')
+        print('=> Modelo LSTM')
         for key, layer in enumerate(self.layers):
             print('Camada %i: ' % (key + 1), end='')
             print('qtd_neurons: %i; ' % layer['qtd_neurons'], end='')
@@ -199,38 +148,38 @@ class ModelLSTM:
         # Apresentação dos gráficos de treinamento e validação da rede
         Path('graphics').mkdir(parents=True, exist_ok=True)
 
-        # plt.plot(history.history['rmseMetric'])
-        # plt.plot(history.history['val_rmseMetric'])
-        # plt.title('RMSE - Treinamento e validação')
-        # plt.xlabel('Épocas')
-        # plt.ylabel('RMSE')
-        # plt.legend(['Treinamento', 'Validação'], loc='upper left')
-        # plt.savefig('graphics/mlp_rmse.png')
-        # plt.close()
-        #
-        # plt.plot(history.history['mape'])
-        # plt.plot(history.history['val_mape'])
-        # plt.title('MAPE')
-        # plt.xlabel('Épocas')
-        # plt.ylabel('MAPE')
-        # plt.legend(['Treinamento', 'Validação'], loc='upper left')
-        # plt.savefig('graphics/mlp_mape.png')
-        # plt.close()
-        #
-        # plt.plot(history.history['accuracy'])
-        # plt.plot(history.history['val_accuracy'])
-        # plt.title('R2 - Treinamento e validação')
-        # plt.xlabel('Épocas')
-        # plt.ylabel('R2')
-        # plt.legend(['Treinamento', 'Validação'], loc='upper left')
-        # plt.savefig('graphics/mlp_r2.png')
-        # plt.close()
+        plt.plot(history.history['rmseMetric'])
+        plt.plot(history.history['val_rmseMetric'])
+        plt.title('RMSE - Treinamento e validação')
+        plt.xlabel('Épocas')
+        plt.ylabel('RMSE')
+        plt.legend(['Treinamento', 'Validação'], loc='upper left')
+        plt.savefig('graphics/mlp_rmse.png')
+        plt.close()
+
+        plt.plot(history.history['mape'])
+        plt.plot(history.history['val_mape'])
+        plt.title('MAPE')
+        plt.xlabel('Épocas')
+        plt.ylabel('MAPE')
+        plt.legend(['Treinamento', 'Validação'], loc='upper left')
+        plt.savefig('graphics/mlp_mape.png')
+        plt.close()
+
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('R2 - Treinamento e validação')
+        plt.xlabel('Épocas')
+        plt.ylabel('R2')
+        plt.legend(['Treinamento', 'Validação'], loc='upper left')
+        plt.savefig('graphics/mlp_r2.png')
+        plt.close()
 
     def predict(self):
         """
-        Método que realiza todas as etapas para detecção de Fake News com o modelo MLP
+        Método que realiza todas as etapas para detecção de Fake News com o modelo
         """
-        print('Iniciando a detecção de fake news com o modelo MLP... ')
+        print('Iniciando a detecção de fake news com o modelo LSTM... ')
 
         print('Criando o modelo... ')
         self.__create_model()
@@ -239,9 +188,8 @@ class ModelLSTM:
         history = self.__train()
 
         print('Testando o modelo o modelo... ')
-        # loss, accuracy_model, rmse, mape, accuracy_detection = self.__test()
-        a = self.__test()
+        loss, accuracy_model, rmse, mape, accuracy_detection = self.__test()
 
         print('Detecção de fake news realizada com sucesso! ')
         print('\nResultados: ')
-        # self.__result(history, loss, accuracy_model, rmse, mape, accuracy_detection)
+        self.__result(history, loss, accuracy_model, rmse, mape, accuracy_detection)
