62ee436c7565c50f17430c44d625f3796f8a1ca0
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2021-01-29 08:46:58 -0300

    [ADD] Iniciado testes com a quantidade de neurônios variada

--- script_02_detection.py
+++ script_02_detection.py
@@ -55,48 +55,47 @@ def generateMLP(data, dataset_name):
     :param dataset_name: Nome do dataset utilizado
     :type dataset_name: str
     """
+    # # TESTES COM FUNÇÕES DE ATIVAÇÃO VARIADAS
+    # model = {
+    #     'epochs': 50,
+    #     'batch_size': 10,
+    #     'layers': [
+    #         {'qtd_neurons': 12, 'activation': Model.ATIVACAO_RELU},
+    #         {'qtd_neurons': 8, 'activation': Model.ATIVACAO_RELU},
+    #         {'qtd_neurons': 1, 'activation': Model.ATIVACAO_SIGMOID},
+    #     ]
+    # }
+    # funcoes = [Model.ATIVACAO_SIGMOID, Model.ATIVACAO_TANH, Model.ATIVACAO_RELU, Model.ATIVACAO_ELU]
+    # for ativacao_atual_entrada in funcoes:
+    #     model['layers'][0]['activation'] = ativacao_atual_entrada
+    #     for ativacao_atual_intermediaria in funcoes:
+    #         model['layers'][1]['activation'] = ativacao_atual_intermediaria
+    #         for ativacao_atual_saida in funcoes:
+    #             model['layers'][2]['activation'] = ativacao_atual_saida
+    #             print('\nmodel:')
+    #             print(model)
+    #             model_mlp = ModelMLP(model, data, dataset_name)
+    #             model_mlp.predict()
+
+    # TESTES COM QUANTIDADE DE NEURÔNIOS VARIADA
     model = {
-        'epochs': 50,
-        'batch_size': 10,
-        'layers': [
-            # camada de entrada
-            {
-                'qtd_neurons': 12,
-                'activation': Model.ATIVACAO_RELU,
-            },
-            # camada intermediária 01
-            {
-                'qtd_neurons': 8,
-                'activation': Model.ATIVACAO_RELU,
-            },
-            # camada de saída
-            {
-                'qtd_neurons': 1,
-                'activation': Model.ATIVACAO_SIGMOID,
-            },
+        "epochs": 50,
+        "batch_size": 10,
+        "layers": [
+            {"qtd_neurons": 2, "activation": "tanh"},
+            {"qtd_neurons": 8, "activation": "relu"},
+            {"qtd_neurons": 1, "activation": "tanh"}
         ]
     }
-
-    # TESTES COM FUNÇÕES DE ATIVAÇÃO VARIADAS
-    funcoes = [
-        Model.ATIVACAO_SIGMOID,
-        Model.ATIVACAO_TANH,
-        Model.ATIVACAO_RELU,
-        Model.ATIVACAO_ELU,
-    ]
-    for ativacao_atual_entrada in funcoes:
-        model['layers'][0]['activation'] = ativacao_atual_entrada
-        for ativacao_atual_intermediaria in funcoes:
-            model['layers'][1]['activation'] = ativacao_atual_intermediaria
-            for ativacao_atual_saida in funcoes:
-                model['layers'][2]['activation'] = ativacao_atual_saida
-                print('\nmodel:')
-                print(model)
-                model_mlp = ModelMLP(model, data, dataset_name)
-                model_mlp.predict()
-
-    model_mlp = ModelMLP(model, data, dataset_name)
-    model_mlp.predict()
+    neuronios = [1, 2, 4, 8, 10, 25, 30, 50, 75, 90, 100, 150, 200, 300]
+    for neuronios_atual_entrada in neuronios:
+        model['layers'][0]['qtd_neurons'] = neuronios_atual_entrada
+        for neuronios_atual_intermediaria in neuronios:
+            model['layers'][1]['qtd_neurons'] = neuronios_atual_intermediaria
+            print('\nmodel:')
+            print(model)
+            model_mlp = ModelMLP(model, data, dataset_name)
+            model_mlp.predict()
 
 
 def generateLSTM(data, dataset_name):
